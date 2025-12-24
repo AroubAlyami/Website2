112a6dc16bffdb19d9754842058223df0700222c
AroubAlyami <67063684+AroubAlyami@users.noreply.github.com>
2025-12-24 16:59:23 +0300

    Create Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Nav({ secretUnlocked }) {
+  return (
+    <nav className="nav">
+      <NavLink to="/" end>Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/sunmoon">Sun & Moon</NavLink>
+      <NavLink to="/laughs">Laughs</NavLink>
+      <NavLink to="/texts">Texts</NavLink>
+      <NavLink to="/learned">Learned</NavLink>
+      <NavLink to="/gift">Gift</NavLink>
+      {secretUnlocked && <NavLink to="/secret">Secret</NavLink>}
+    </nav>
+  );
+}
