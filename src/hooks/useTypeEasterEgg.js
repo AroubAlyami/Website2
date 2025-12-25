453d52014a3ad69219e2ce261b8ca31e7492cad1
AroubAlyami <67063684+AroubAlyami@users.noreply.github.com>
2025-12-26 02:00:21 +0300

    Update useTypeEasterEgg.js

--- src/hooks/useTypeEasterEgg.js
+++ src/hooks/useTypeEasterEgg.js
@@ -1,17 +1,16 @@
 import { useEffect, useRef } from "react";
 
-export default function useTypeEasterEgg({ onSunset, onMoon, onWe }) {
+export default function useTypeEasterEgg({ onSunset, onMoon, onWe, onCats }) {
   const buffer = useRef("");
 
   useEffect(() => {
     function onKey(e) {
-      // ignore modifiers
       if (e.metaKey || e.ctrlKey || e.altKey) return;
 
       const k = e.key;
       if (k.length !== 1) return;
 
-      buffer.current = (buffer.current + k).slice(-20).toLowerCase();
+      buffer.current = (buffer.current + k).slice(-30).toLowerCase();
 
       if (buffer.current.includes("sunset")) {
         onSunset?.();
@@ -21,13 +20,16 @@ export default function useTypeEasterEgg({ onSunset, onMoon, onWe }) {
         onMoon?.();
         buffer.current = "";
       }
+      if (buffer.current.includes("cats")) {
+        onCats?.();
+        buffer.current = "";
+      }
       if (buffer.current.includes("we")) {
         onWe?.();
-        // don’t clear, just ping
       }
     }
 
     window.addEventListener("keydown", onKey);
     return () => window.removeEventListener("keydown", onKey);
-  }, [onMoon, onSunset, onWe]);
+  }, [onMoon, onSunset, onWe, onCats]);
 }
