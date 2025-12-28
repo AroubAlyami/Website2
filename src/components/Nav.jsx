f4a08f3764520413e1564127a4ad8544803dce19
AroubAlyami <67063684+AroubAlyami@users.noreply.github.com>
2025-12-28 09:53:05 +0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function Nav({ secretUnlocked, catsUnlocked }) {
+export default function Nav({ secretUnlocked }) {
   return (
     <nav className="nav">
       <NavLink to="/" end>Home</NavLink>
