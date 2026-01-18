a878094adbe6ec3a261b0f2741155630c1f99cc6
AroubAlyami <67063684+AroubAlyami@users.noreply.github.com>
2026-01-18 12:34:16 +0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,17 +6,13 @@ export default function Nav({ secretUnlocked }) {
     <nav className="nav">
       <NavLink to="/" end>Home</NavLink>
       <NavLink to="/about">About</NavLink>
-      <NavLink to="/sunmoon">Sun & Moon</NavLink>
-      <NavLink to="/laughs">Laughs</NavLink>
       <NavLink to="/texts">Texts</NavLink>
       <NavLink to="/learned">Learned</NavLink>
       <NavLink to="/gift">Gift</NavLink>
       <NavLink to="/cats">Cats</NavLink>
-      <NavLink to="/timeline">Timeline</NavLink>
       <NavLink to="/compliments">Compliments</NavLink>
       <NavLink to="/art-gallery">Art gallery</NavLink>
       <NavLink to="/yesno">Yes/No</NavLink>
-      <NavLink to="/kanye">Kanye</NavLink>
       <NavLink to="/do-something">Do something</NavLink>
 
 
